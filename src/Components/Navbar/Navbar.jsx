eac5b3ef100c6e56ecff19ce8f93692e99d5aa35
hardik18-hk19 <hardikagarwal9308@gmail.com>
2024-08-29 03:22:33 +0530

    Added responsiveness to Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./Navbar.css";
 
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from "../Assets/nav_dropdown.png";
 
 //Navbar component to display the website logo and navigation links.
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
   const { getTotalCartItems } = useContext(ShopContext);
-
+  const menuRef = useRef();
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="Logo" />
         <p>SHOPPER</p>
       </div>
-      <ul className="nav-menu">
+      <img src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className="nav-menu">
         <li
           onClick={() => {
             setMenu("shop");
